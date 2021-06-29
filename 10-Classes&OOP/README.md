a494862941ae6892e5ba1e9f717ddf19270f5658
ernestoivan-tzompantzimoreno <ernestoivan.tzompantzimoreno@vodafone.com>
2021-06-29 18:13:34 +0200

    UPDATES on Classes and Objects module

--- 10-Classes&OOP/README.md
+++ 10-Classes&OOP/README.md
@@ -1,6 +1,6 @@
 ## Fields vs properties
 
-When working with classes we can use fields on the constructor that have not been declare before inside the object. The constructor will create that field and asign the indicated value:
+When working with classes we can use fields on the constructor that have not been declare before inside the object. The constructor will create that field and assign the indicated value:
 
 ```JavaScript
 class ProductItem {
@@ -11,5 +11,106 @@ class ProductItem {
         this.price = price;
     }
 }
-// The constructor will create every field and assign the value. We can use both syntax (similar to C# or this one).
+// The constructor will create every field and assign the value. We can use this way or the syntax similar to C#.
 ```
+
+We can also declare a new field inside a method.
+
+```JavaScript
+class Shop {
+    render() {
+        const renderHook = document.getElementById('app'); //Normal variable inside the method render()
+        this.cart = new ShoppingCart(); //Creates a field on the Shop object and indicates its value.
+        const cartEl = cart.render();
+        const productList = new ProductList();
+        const prodListEl = productList.render();
+        
+        renderHook.append(cartEl);
+        renderHook.append(prodListEl);
+    }
+}
+```
+
+## 246. Binding methods and working with `this`
+On the following code, the event listener is referring to the button itself. The button is activating the method, that is why we see on the console.log "`undefined`". We want to print the object to which the button belongs to, we use `bind()` to obtain that result.
+
+```JavaScript
+class ProductItem {
+    constructor(product){
+        this.product = product;
+    }
+
+    addToCart(){
+        console.log("Adding product to cart");
+        console.log(this.product);
+    }
+
+    render(){
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
+        <div>
+            <img src="${this.product.imageUrl}" alt="${this.product.title}">
+            <div class = "product-item__content">
+                <h2>${this.product.title}</h2>
+                <h3>\$${this.product.price}</h3>
+                <p>${this.product.description}</p>
+                <button>Add to Cart</button>
+            </div>
+        </div>        
+        `;
+        const addCartButton = prodEl.querySelector('button');
+        addCartButton.addEventListener('click', this.addToCart); // "this" points to the button  not to the object.
+        return prodEl;
+    }
+}
+```
+
+The solution is to bind `this` on the event listener to the correct object:
+
+```JavaScript
+const addCartButton = prodEl.querySelector('button');
+        addCartButton.addEventListener('click', this.addToCart.bind(this)); // "this" inside bind() refers to the current object, the actual object and not any more to the button.
+        return prodEl;
+```
+
+## 249. Static Methods & Properties
+
+- They are defined using the `static` keyword.
+- Accessible on the class itself without instantiation (no object creation).
+- Typically used in helper classes, global configuration.
+
+> Non static classes, methods or properties are used for core, re-usable logic.
+
+### Advantages of using static objects/methods
+
+One of the advantages of using static methods is that we can use the same object for different operations, we can use an object which holds all the necessary static methods.
+
+```JavaScript
+class App{
+    //Method used to start the App itself
+    static init (){
+        const shop = new Shop();
+        this.cart = shop.cart; 
+        shop.render();
+    }
+
+    // Method used to communicate between classes and update view. 
+    static addProductToCart(product){
+        this.cart.addProduct(product);
+    }
+}
+```
+
+## Object literal notation ({}) vs Classes
+
+Object literal notation is very useful for: 
+- Data collection
+- Objects which I only need to create once
+- Quick and easy to create. No overhead.
+
+
+
+Classes should be consider when: 
+- An object needs to be re-created over and over. 
+- Object duplication is easier.
--- 10-Classes&OOP/oop-01-starting-setup/assets/scripts/app.js
+++ 10-Classes&OOP/oop-01-starting-setup/assets/scripts/app.js
@@ -1,47 +1,117 @@
-class ProductItem {
+// This class is understood as a data container.
+class Product {
     title = 'DEFAULT';
     imageUrl;
     description;
     price;
 
-    constructor(title, imageUrl, description, price) {
+    constructor(title, imageUrl, price, description ) {
         this.title = title;
         this.imageUrl = imageUrl;
-        this.description = description;
         this.price = price;
+        this.description = description;
     }
 }
 
-const productList = {
-    products : [
-        new ProductItem('A pillow','https://www.midwives2020.org/wp-content/uploads/2021/02/719-00201_main-shot_01_therapur-cool-pillow.jpg', 19.99, 'A soft pillow'), 
-        new ProductItem('Carpet', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Wollteppich_1.jpg/1200px-Wollteppich_1.jpg', 89.99, 'A carpet you might like')
-    ],
-    
-    render() {
-        const renderHook = document.getElementById('app');
+// This class is a logic container 
+class ProductItem {
+    constructor(product){
+        this.product = product;
+    }
+
+    addToCart(){
+        console.log("Adding product to cart");
+        console.log(this.product);
+        App.addProductToCart(this.product);
+    }
+
+    render(){
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
+        <div>
+            <img src="${this.product.imageUrl}" alt="${this.product.title}">
+            <div class = "product-item__content">
+                <h2>${this.product.title}</h2>
+                <h3>\$${this.product.price}</h3>
+                <p>${this.product.description}</p>
+                <button>Add to Cart</button>
+            </div>
+        </div>        
+        `;
+        const addCartButton = prodEl.querySelector('button');
+        // addCartButton.addEventListener('click', this.addToCart); // Check documentation 246.
+        addCartButton.addEventListener('click', this.addToCart.bind(this));
+        return prodEl;
+    }
+}
+
+// This class is a logic container
+class ProductList {
+    products = [
+        new Product('A pillow','https://www.midwives2020.org/wp-content/uploads/2021/02/719-00201_main-shot_01_therapur-cool-pillow.jpg', 19.99, 'A soft pillow'), 
+        new Product('Carpet', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Wollteppich_1.jpg/1200px-Wollteppich_1.jpg', 89.99, 'A carpet you might like')
+    ];
+
+    render () {
+        
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for(const prod of this.products)
         {
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-            <div>
-                <img src="${prod.imageUrl}" alt="${prod.title}">
-                <div class = "product-item__content">
-                    <h2>${prod.title}</h2>
-                    <h3>\$${prod.price}</h3>
-                    <p>${prod.description}</p>
-                    <button>Add to Cart</button>
-                </div>
-            </div>        
-            `;
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         }
+        return prodList;
+    };
+}
 
-        renderHook.append(prodList);
+class ShoppingCart {
+    items = [];
+    
+    addProduct(product) {
+        this.items.push(product);
+        this.totalOutput.innerHTML = `<h2>Total \$${1}</h2>`;
     }
-};
 
-productList.render();
+    render() {
+        const cartEl = document.createElement('section');
+        cartEl.innerHTML = `
+            <h2>Total \$${0}</h2>
+            <button>Order Now!</button>
+        `;
+        cartEl.className = 'cart';
+        this.totalOutput = cartEl.querySelector('h2');
+        return cartEl;
+    };
+}
+
+class Shop {
+    render() {
+        const renderHook = document.getElementById('app');
+        this.cart = new ShoppingCart();
+        const cartEl = this.cart.render();
+        const productList = new ProductList();
+        const prodListEl = productList.render();
+        
+        renderHook.append(cartEl);
+        renderHook.append(prodListEl);
+    }
+}
+
+class App{
+    static cart;
+
+    static init (){
+        const shop = new Shop();
+        shop.render();
+        this.cart = shop.cart; //Creates a cart property on App, referring to the cart property on the Shop object created above.
+    }
+
+    static addProductToCart(product){
+        this.cart.addProduct(product);
+    }
+}
+
+App.init();
